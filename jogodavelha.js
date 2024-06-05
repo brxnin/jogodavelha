9db6dd6c72cc29af0f6fc299d520eb57d8a075ac
brxnin <125562306+brxnin@users.noreply.github.com>
2024-06-05 08:46:59 -0300

    Adicionando titulo

--- jogodavelha.js
+++ jogodavelha.js
@@ -1,5 +1,6 @@
 const board = document.getElementById("board")
 const casinhas = board.getElementsByTagName("div")
+const boxVencedor = document.getElementById("vencedor")
 
 let jogadas = 0;
 
@@ -9,10 +10,49 @@ for (let i=0; i<casinhas.length; i++) {
 }
 
 function casinhaclick() {
-    if(jogadas%2 == 0) {
-        this.innerHTML = "X";
-    }else{
-        this.innerHTML = "O";
+    if(this.innerHTML == "") {
+        if(jogadas%2 == 0) {
+            this.innerHTML = "X";
+        }else{
+            this.innerHTML = "O";
+        }
+        jogadas +=1;    
     }
-    jogadas +=1;
+    if(jogadas >=5){
+        verificaGanhador()
+    }
+}
+
+function verificaGanhador() {
+    //validando na horizontal
+    if(casinhas[0].innerHTML == casinhas[1].innerHTML && casinhas[1].innerHTML == casinhas[2].innerHTML) {
+        boxVencedor.innerHTML = "O '" + casinhas[0].innerHTML + "' Venceu!"
+    }
+    if(casinhas[3].innerHTML == casinhas[4].innerHTML && casinhas[4].innerHTML == casinhas[5].innerHTML) {
+        alert("O '" + casinhas[3].innerHTML + "' Venceu!")
+    }
+    if(casinhas[6].innerHTML == casinhas[7].innerHTML && casinhas[7].innerHTML == casinhas[8].innerHTML) {
+        alert("O '" + casinhas[6].innerHTML + "' Venceu!")
+    }
+
+    //validando na vertical
+    if(casinhas[0].innerHTML == casinhas[3].innerHTML && casinhas[3].innerHTML == casinhas[6].innerHTML) {
+        alert("O '" + casinhas[0].innerHTML + "' Venceu!")
+    }
+    if(casinhas[1].innerHTML == casinhas[4].innerHTML && casinhas[4].innerHTML == casinhas[7].innerHTML) {
+        alert("O '" + casinhas[1].innerHTML + "' Venceu!")
+    }
+    if(casinhas[2].innerHTML == casinhas[5].innerHTML && casinhas[5].innerHTML == casinhas[8].innerHTML) {
+        alert("O '" + casinhas[2].innerHTML + "' Venceu!")
+    }
+
+    //validando na vertical
+    if(casinhas[0].innerHTML == casinhas[4].innerHTML && casinhas[4].innerHTML == casinhas[8].innerHTML) {
+        alert("O '" + casinhas[0].innerHTML + "' Venceu!")
+    }
+    if(casinhas[2].innerHTML == casinhas[4].innerHTML && casinhas[4].innerHTML == casinhas[6].innerHTML) {
+        alert("O '" + casinhas[2].innerHTML + "' Venceu!")
+    }
+
+    console.log(casinhas[0].innerHTML)
 }
