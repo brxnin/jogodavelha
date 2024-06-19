94b590e3b64b30b0abebd8fc4cd9125f049aac45
brxnin <125562306+brxnin@users.noreply.github.com>
2024-06-19 08:20:17 -0300

    Update jogodavelha.js

--- jogodavelha.js
+++ jogodavelha.js
@@ -1,125 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link href="jogodavelha.css" rel="stylesheet" type="text/css" />
-  <style>
-    #board {
-      border: thin solid red;
-      width: 420px;
-      height: 420px;
-      overflow: hidden;
+const board = document.getElementById("board");
+const casinhas = board.getElementsByClassName("casinha");
+const boxVencedor = document.getElementById("vencedor");
+
+let jogadas = 0;
+let jogoAcabou = false;
+
+for (let i = 0; i < casinhas.length; i++) {
+  casinhas[i].addEventListener('click', casinhaclick);
+}
+
+function casinhaclick() {
+  if (jogoAcabou || this.innerHTML !== "") return; // Não permitir jogadas se o jogo já acabou ou a casa já estiver preenchida
+
+  if (jogadas % 2 === 0) {
+    this.innerHTML = "X";
+  } else {
+    this.innerHTML = "O";
+  }
+  jogadas += 1;
+  
+  verificaGanhador();
+}
+
+function verificaGanhador() {
+  const linhas = [
+    [0, 1, 2], // primeira linha
+    [3, 4, 5], // segunda linha
+    [6, 7, 8], // terceira linha
+    [0, 3, 6], // primeira coluna
+    [1, 4, 7], // segunda coluna
+    [2, 5, 8], // terceira coluna
+    [0, 4, 8], // diagonal principal
+    [2, 4, 6]  // diagonal secundária
+  ];
+
+  for (let linha of linhas) {
+    const [a, b, c] = linha;
+    if (casinhas[a].innerHTML !== "" &&
+        casinhas[a].innerHTML === casinhas[b].innerHTML &&
+        casinhas[b].innerHTML === casinhas[c].innerHTML) {
+      boxVencedor.innerHTML = "O '" + casinhas[a].innerHTML + "' Venceu!";
+      jogoAcabou = true;
+      return;
     }
+  }
 
-    .casinha {
-      width: 135px;
-      height: 135px;
-      border: thin solid;
-      float: left;
-      text-align: center;
-      line-height: 135px;
-      font-size: 80px;
-    }
-
-    .gray {
-      background: black;
-      color: #ffffff;
-    }
-  </style>
-</head>
-<body>
-  <h1>Jogo da Velha</h1>
-  <div id="board">
-    <div class="casinha"></div>
-    <div class="casinha gray"></div>
-    <div class="casinha"></div>
-    <div class="casinha gray"></div>
-    <div class="casinha"></div>
-    <div class="casinha gray"></div>
-    <div class="casinha"></div>
-    <div class="casinha gray"></div>
-    <div class="casinha"></div>
-  </div>
-  <div id="vencedor"></div>
-  <script type="text/javascript">
-    const board = document.getElementById("board");
-    const casinhas = board.getElementsByClassName("casinha");
-    const boxVencedor = document.getElementById("vencedor");
-
-    let jogadas = 0;
-    let jogoAcabou = false;
-
-    for (let i = 0; i < casinhas.length; i++) {
-      casinhas[i].addEventListener('click', casinhaclick);
-    }
-
-    function casinhaclick() {
-      if (jogoAcabou) return; // Não permitir mais jogadas se o jogo já acabou
-
-      if (this.innerHTML === "") {
-        if (jogadas % 2 === 0) {
-          this.innerHTML = "X";
-        } else {
-          this.innerHTML = "O";
-        }
-        jogadas += 1;
-        verificaGanhador();
-      }
-    }
-
-    function verificaGanhador() {
-      // Validar na horizontal
-      for (let i = 0; i <= 6; i += 3) {
-        if (casinhas[i].innerHTML !== "" &&
-            casinhas[i].innerHTML === casinhas[i + 1].innerHTML &&
-            casinhas[i + 1].innerHTML === casinhas[i + 2].innerHTML) {
-          boxVencedor.innerHTML = "O '" + casinhas[i].innerHTML + "' Venceu!";
-          jogoAcabou = true;
-          return;
-        }
-      }
-
-      // Validar na vertical
-      for (let i = 0; i <= 2; i++) {
-        if (casinhas[i].innerHTML !== "" &&
-            casinhas[i].innerHTML === casinhas[i + 3].innerHTML &&
-            casinhas[i + 3].innerHTML === casinhas[i + 6].innerHTML) {
-          boxVencedor.innerHTML = "O '" + casinhas[i].innerHTML + "' Venceu!";
-          jogoAcabou = true;
-          return;
-        }
-      }
-
-      // Validar na diagonal
-      if (casinhas[0].innerHTML !== "" &&
-          casinhas[0].innerHTML === casinhas[4].innerHTML &&
-          casinhas[4].innerHTML === casinhas[8].innerHTML) {
-        boxVencedor.innerHTML = "O '" + casinhas[0].innerHTML + "' Venceu!";
-        jogoAcabou = true;
-        return;
-      }
-
-      if (casinhas[2].innerHTML !== "" &&
-          casinhas[2].innerHTML === casinhas[4].innerHTML &&
-          casinhas[4].innerHTML === casinhas[6].innerHTML) {
-        boxVencedor.innerHTML = "O '" + casinhas[2].innerHTML + "' Venceu!";
-        jogoAcabou = true;
-        return;
-      }
-
-      // Verificar se todas as casinhas estão preenchidas (empate)
-      let todasPreenchidas = true;
-      for (let i = 0; i < casinhas.length; i++) {
-        if (casinhas[i].innerHTML === "") {
-          todasPreenchidas = false;
-          break;
-        }
-      }
-
-      if (todasPreenchidas) {
-        boxVencedor.innerHTML = "Empate!";
-        jogoAcabou = true;
-      }
-    }
-  </script>
-</body>
-</html>
+  // Verificar se todas as casinhas estão preenchidas (empate)
+  if (jogadas === 9) {
+    boxVencedor.innerHTML = "Empate!";
+    jogoAcabou = true;
+  }
+}
